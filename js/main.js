0c8c17bdba7b2df68c425e3e6fe9ed0e0406b0fe
Кирпиченков Никита <kirpich.nikita98@mail.ru>
2025-04-16 23:16:55 +0300

    генерация массива из 25 обьектов по ТЗ

---
+++ js/main.js
@@ -0,0 +1,106 @@
+const CARDS_COUNT = 25;
+
+const DESCRIPTIONS = [
+  'Горы Китая',
+  'Котопес',
+  'Жизнь у моря',
+  'Солнечная лагуна',
+];
+
+const COMMENTS_ID_COUNT = {
+  MIN: 0,
+  MAX: 250,
+};
+
+const LIKES_COUNT = {
+  MIN: 15,
+  MAX: 250,
+};
+
+const AVATAR_COUNT = {
+  MIN: 0,
+  MAX: 6,
+};
+
+const URL_COUNT = {
+  MIN: 1,
+  MAX: 25,
+};
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+//генерация рандомного числа от min до max
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//генерация чисел от min до max по порядку
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+const generatePhotoId = createIdGenerator();
+
+//генерация числа из заданого промежутка без повторения значений
+function createRandomNumberFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const generateUrlNumber = createRandomNumberFromRangeGenerator(URL_COUNT.MIN, URL_COUNT.MAX);
+
+//взятие рандомного элемента из массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+//генерируем массив с описанием фотографии
+const createPhotoDescription = () => ({
+  id: generatePhotoId(),
+  url: `photos/${generateUrlNumber()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LIKES_COUNT.MIN, LIKES_COUNT.MAX),
+  comments:{
+    id: getRandomInteger(COMMENTS_ID_COUNT.MIN, COMMENTS_ID_COUNT.MAX),
+    name: getRandomArrayElement(NAMES),
+    avatar: `img/avatar-${getRandomInteger(AVATAR_COUNT.MIN, AVATAR_COUNT.MAX)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+  },
+});
+
+//создаем массив из 25 обьектов
+const getCards = Array.from({length: CARDS_COUNT}, createPhotoDescription);
+
+getCards(); //для линтера
